e139e29cfc725bd5f6309049982c216cbeece8e0
mufaddal <muffizone@gmail.com>
2021-04-15 10:26:20 +0000

    changed from dropbox to freeform fields for batsmen and bowlers in the match-statistics admin age

--- cricbox/match_statistics/admin.py
+++ cricbox/match_statistics/admin.py
@@ -15,10 +15,12 @@ class ResultAdmin(admin.ModelAdmin):
 
 class BatsmanAdmin(admin.TabularInline):
     model = Batsman
+    autocomplete_fields = ("player")
 
 
 class BowlerAdmin(admin.TabularInline):
     model = Bowler
+    autocomplete_fields = ("player")
 
 
 class MatchStatisticsAdmin(admin.ModelAdmin):
@@ -26,6 +28,7 @@ class MatchStatisticsAdmin(admin.ModelAdmin):
     list_filter = ["match__date", "match__mtype", "match__home_or_away", "result"]
     date_hierarchy = "match__date"
     inlines = (BatsmanAdmin, BowlerAdmin)
+
 
     def date(self, x):
         return x.match.date
