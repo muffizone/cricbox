f32d99b7208c65891d07e9d51eb4201cc272e5f9
mufaddal <muffizone@gmail.com>
2021-04-23 01:24:59 +0000

    updated the fields as per Laurie's input

--- cricbox/match_statistics/models.py
+++ cricbox/match_statistics/models.py
@@ -21,19 +21,19 @@ class MatchStatistics(models.Model):
     match = models.OneToOneField(Match, on_delete=models.CASCADE, null=True)
     result = models.ForeignKey(Result, verbose_name="Result", on_delete=models.PROTECT, null=True)
     report = models.TextField("Report", blank=True, null=True)
-    london_fields_overs = models.DecimalField("London Fields Overs", max_digits=3, decimal_places=1, null=True)
+    london_fields_overs = models.DecimalField("London Fields Overs Bowled", max_digits=3, decimal_places=1, null=True)
     london_fields_score = models.PositiveSmallIntegerField("London Fields Score", null=True)
-    london_fields_wickets = models.PositiveSmallIntegerField("London Fields Wickets", null=True)
+    london_fields_wickets = models.PositiveSmallIntegerField("London Fields Wickets Taken", null=True)
     opposition_score = models.PositiveSmallIntegerField("Opposition score", null=True)
-    opposition_wickets = models.PositiveSmallIntegerField("Opposition Wickets", null=True)
-    opposition_overs = models.DecimalField("Opposition overs", max_digits=3, decimal_places=1, null=True)
+    opposition_wickets = models.PositiveSmallIntegerField("Opposition Wickets Taken", null=True)
+    opposition_overs = models.DecimalField("Opposition overs Bowled", max_digits=3, decimal_places=1, null=True)
     london_fields_first_event = models.PositiveSmallIntegerField(choices=EVENT_FIRST, default=0, null=True)
     report_byline = models.CharField(max_length=200, null=True)
     report_author = models.CharField(max_length=200, null=True)
     report_headline = models.CharField(max_length=200, null=True)
 
     def get_bowlers(self):
-        return self.bowler_set.get_queryset()
+        return self.bowler_set.get_queryset().order_by('number')
 
     def get_batsman(self):
         return self.batsman_set.get_queryset()
